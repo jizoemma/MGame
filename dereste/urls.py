9f3e2c0175e9d90f0fc810b7cc3a9783657257cb
jizoemma <jizoemma@gmail.com>
2020-01-13 14:37:29 +0900

    all list と pagination を追加

--- dereste/urls.py
+++ dereste/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import DeresteIndexView
+from .views import DeresteIndexView, AllListView
 
 urlpatterns = [
   #path('', views.index, name='index'),
   path('', DeresteIndexView.as_view(), name='dereste'),
+  path('allList/', AllListView.as_view(), name='allList'),
 ]
--- dereste/views.py
+++ dereste/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Songs
+
 # Create your views here.
 
 class DeresteIndexView(TemplateView):
   template_name = 'dereste_index.html'
 
+class AllListView(ListView):
+  model = Songs
+  paginate_by = 20
+  template_name = 'dereste_all_list.html'
