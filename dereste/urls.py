eef0326ab9756336ca7bb1ec0b104841802d6e75
jizoemma <jizoemma@gmail.com>
2020-02-09 21:27:48 +0900

    Challenges delete 実装

--- dereste/urls.py
+++ dereste/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
-from .views import DeresteIndexView,AllListView,SongCreate,SongUpdate,SongDelete,ChallengesCreate
+from .views import DeresteIndexView, AllListView, SongCreate, SongUpdate, SongDelete, ChallengesCreate
 from dereste import views
 
 urlpatterns = [
-  #path('', views.index, name='index'),
+  # path('', views.index, name='index'),
   path('', DeresteIndexView.as_view(), name='dereste'),
-  #path('allList/', AllListView.as_view(), name='allList'),
+  # path('allList/', AllListView.as_view(), name='allList'),
   path('allList/', views.song_all_list, name='allList'),
   path('create/', views.SongCreate.as_view(), name='create'),
   path('delete/<int:pk>/', views.SongDelete.as_view(), name='delete'),
   path('update/<int:pk>/', views.SongUpdate.as_view(), name='update'),
   path('ch_create/', views.ChallengesCreate.as_view(), name='ch_create'),
-  path('ch_allList/', views.challenges_all_list, name='challList'),
+  path('ch_allList/', views.challenges_all_list, name='ch_allList'),
+  path('ch_delete/<int:pk>/', views.ChallengesDelete.as_view(), name='ch_delete'),
 ]
--- dereste/views.py
+++ dereste/views.py
@@ -1,60 +1,76 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic.edit import DeleteView ,UpdateView
+from django.views.generic.edit import DeleteView, UpdateView
 from .models import Songs, Challenges
-from .filters import SongsFilter,ChallengesFilter
+from .filters import SongsFilter, ChallengesFilter
 from .forms import CreateSongForm, CreateChallengesForm
 from django.urls import reverse_lazy, reverse
 from django.views.generic import TemplateView, ListView, DetailView, CreateView
 
 # Create your views here.
 
+
 class DeresteIndexView(TemplateView):
-  template_name = 'dereste_index.html'
+    template_name = 'dereste_index.html'
+
 
 class AllListView(ListView):
-  model = Songs
-  paginate_by = 20
-  template_name = 'dereste_all_list.html'
+    model = Songs
+    paginate_by = 20
+    template_name = 'dereste_all_list.html'
 
 # Create
+
+
 class SongCreate(CreateView):
-  model = Songs
-  form_class = CreateSongForm
-  #form_class = CreateHelperForm
-  template_name = "dereste_create.html"
-  success_url = "../allList"
+    model = Songs
+    form_class = CreateSongForm
+    # form_class = CreateHelperForm
+    template_name = "dereste_create.html"
+    success_url = "../allList"
 
 # Update
+
+
 class SongUpdate(UpdateView):
-  model = Songs
-  fields = ['name', 'level','type', 'notes','grade']
-  template_name = "songs_update_form.html"
-  success_url = reverse_lazy('allList')
+    model = Songs
+    fields = ['name', 'level', 'type', 'notes', 'grade']
+    template_name = "songs_update_form.html"
+    success_url = reverse_lazy('allList')
 
 # songs all List
+
+
 def song_all_list(request):
     f = SongsFilter(request.GET, queryset=Songs.objects.all())
     return render(request, 'dereste_all_list.html', {'filter': f})
 
 # Songs Delete
+
+
 class SongDelete(DeleteView):
-  template_name = 'songs_confirm_delete.html'
-  model = Songs
-  success_url = reverse_lazy('allList')
+    template_name = 'songs_confirm_delete.html'
+    model = Songs
+    success_url = reverse_lazy('allList')
 
 # Challenges Delete
+
+
 class ChallengesDelete(DeleteView):
-  template_name = 'challenges_confirm_delete.html'
-  model = Challenges
-  success_url = reverse_lazy('ch_allList')
+    template_name = 'challenges_confirm_delete.html'
+    model = Challenges
+    success_url = reverse_lazy('ch_allList')
+
+
 class ChallengesCreate(CreateView):
-  model = Challenges
-  form_class = CreateChallengesForm
-  template_name = "dereste_challenges_create.html"
-  success_url = "../ch_allList"
+    model = Challenges
+    form_class = CreateChallengesForm
+    template_name = "dereste_challenges_create.html"
+    success_url = "../ch_allList"
 
 # challenges all List
+
+
 def challenges_all_list(request):
     f = ChallengesFilter(request.GET, queryset=Challenges.objects.all())
     return render(request, 'dereste_challenges_all_list.html', {'filter': f})
